dbd4e652d2e1c029cbb32367a4c77df043aabbe6
Federico Palmieri <fedepalmie95@gmail.com>
2023-05-23 16:13:07 -0300

    fix: change message error

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -2,10 +2,12 @@ const pool = require('../database');
 
 module.exports.getMovies = async (req, res, next) => {
   try {
-    const result = await pool.query('SELECT id, title, description, image FROM movies');
+    const result = await pool.query(
+      'SELECT id, title, description, image FROM movies'
+    );
 
     if (!result.length) {
-      res.status(404).json({ message: 'no Movies found' });
+      res.status(404).json({ message: 'No Movies found' });
       return;
     }
 
@@ -19,7 +21,10 @@ module.exports.addMovie = async (req, res, next) => {
   try {
     const { title, description, image } = req.body;
 
-    await pool.query('INSERT INTO movies (title, description, image) VALUES (?, ?, ?)', [title, description, image]);
+    await pool.query(
+      'INSERT INTO movies (title, description, image) VALUES (?, ?, ?)',
+      [title, description, image]
+    );
     res.status(200).json({ message: 'the movie was added successfully!' });
   } catch (err) {
     next(err);
@@ -29,7 +34,10 @@ module.exports.addMovie = async (req, res, next) => {
 module.exports.modifyMovie = async (req, res, next) => {
   try {
     const { title, description, image } = req.body;
-    const result = await pool.query('UPDATE movies SET title = ?, description = ?, image = ? WHERE id = ? LIMIT 1', [title, description, image, req.params.id]);
+    const result = await pool.query(
+      'UPDATE movies SET title = ?, description = ?, image = ? WHERE id = ? LIMIT 1',
+      [title, description, image, req.params.id]
+    );
 
     if (!result.affectedRows) {
       res.status(404).json({ message: 'The requested movie was not found' });
