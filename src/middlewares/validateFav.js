7745523357394f9d73f3ce74c6f478ef70402d6c
Federico Palmieri <fedepalmie95@gmail.com>
2023-05-23 17:16:08 -0300

    feat: new message validator

--- src/middlewares/validateFav.js
+++ src/middlewares/validateFav.js
@@ -2,11 +2,18 @@ const pool = require('../database');
 
 module.exports.validateFav = async (req, res, next) => {
   try {
-    const result = JSON.parse(JSON.stringify(await pool.query('SELECT COUNT(*) as count FROM favorites WHERE id_movie = ? and id_user = ?', [req.params.id, req.userId])));
+    const result = JSON.parse(
+      JSON.stringify(
+        await pool.query(
+          'SELECT COUNT(*) as count FROM favorites WHERE id_movie = ? and id_user = ?',
+          [req.params.id, req.userId]
+        )
+      )
+    );
     const { count } = result[0];
 
     if (count) {
-      res.status(500).json({ message: 'the movie is already favorites' });
+      res.status(500).json({ message: 'the movie is already favorites test' });
       return;
     }
 
