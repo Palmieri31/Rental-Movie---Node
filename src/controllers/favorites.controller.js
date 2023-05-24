3538eb26da9ca5e981878e97f8f2079bc176696c
Federico Palmieri <fedepalmie95@gmail.com>
2023-05-24 10:37:15 -0300

    fix: change success name

--- src/controllers/favorites.controller.js
+++ src/controllers/favorites.controller.js
@@ -2,7 +2,10 @@ const pool = require('../database');
 
 module.exports.getFavorite = async (req, res, next) => {
   try {
-    const result = await pool.query('SELECT f.id, title, description, image FROM movies as m INNER JOIN favorites as f ON m.id = f.id_movie WHERE id_user = ?', [req.userId]);
+    const result = await pool.query(
+      'SELECT f.id, title, description, image FROM movies as m INNER JOIN favorites as f ON m.id = f.id_movie WHERE id_user = ?',
+      [req.userId]
+    );
     const favorites = JSON.parse(JSON.stringify(result));
 
     if (!favorites.length) {
@@ -18,8 +21,13 @@ module.exports.getFavorite = async (req, res, next) => {
 
 module.exports.addFavorite = async (req, res, next) => {
   try {
-    await pool.query('INSERT INTO favorites (id_movie, id_user) VALUES (?, ?);', [req.params.id, req.userId]);
-    res.status(200).json({ message: 'the movie was added to favorites successfully!' });
+    await pool.query(
+      'INSERT INTO favorites (id_movie, id_user) VALUES (?, ?);',
+      [req.params.id, req.userId]
+    );
+    res
+      .status(200)
+      .json({ message: 'the movie was added to favorites successfully!' });
   } catch (err) {
     next(err);
   }
@@ -27,14 +35,21 @@ module.exports.addFavorite = async (req, res, next) => {
 
 module.exports.deleteFavorite = async (req, res, next) => {
   try {
-    const result = await pool.query('DELETE FROM favorites WHERE id = ? LIMIT 1', [req.params.id]);
+    const result = await pool.query(
+      'DELETE FROM favorites WHERE id = ? LIMIT 1',
+      [req.params.id]
+    );
 
     if (!result.affectedRows) {
-      res.status(404).json({ message: 'The requested favorites movie was not found' });
+      res
+        .status(404)
+        .json({ message: 'The requested favorites movie was not found' });
       return;
     }
 
-    res.status(200).json({ message: 'the movie was delete from favorites successfully!' });
+    res
+      .status(200)
+      .json({ message: 'the movie was delete from favorites success!' });
   } catch (err) {
     next(err);
   }
